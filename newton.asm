4929af1a2beb16188ef9a4087dffba4d0d4e07f2
yazuc <51753599+yazuc@users.noreply.github.com>
2024-04-13 12:33:53 -0300

    primeira versão funcional

--- newton.asm
+++ newton.asm
@@ -1,11 +1,10 @@
 .data
-    prompt1: .asciiz "Programa de Raiz Quadrada – Newton-Raphson\n"
+    prompt1: .asciiz "Programa de Raiz Quadrada  Newton-Raphson\n"
     prompt2: .asciiz "Desenvolvedores: Leonardo T. Rubert\n"
     prompt3: .asciiz "Terminou de rodar a recursao\n"
+    prompt4: .asciiz "Insira um valor para x:"
+    prompt5: .asciiz "Insira um valor para i:"
     result_prompt: .asciiz "%f\n"
-    x: .word 100
-    i: .word 4
-
 .text
 .globl main
 main:
@@ -18,54 +17,68 @@ main:
     la $a0, prompt2              # printa desenvolvedor
     syscall
     
-    addi	$sp, $sp, -12	 #  Aloca espaço na pilha para 3 palavras (12 bytes)
-
-    # Calculate sqrt_nr(100, 2)
-    lw $t0, x                    # coloca valor de x em t0
-    sw $t0, 8($sp)		 # coloca x na pilha    
+    li $v0, 4                    
+    la $a0, prompt4              # printa prompt para usuário inserir x
+    syscall
     
-    lw $t1, i                    # coloca valor de i em t1
-    sw $t1, 4($sp) 		 # coloca i na pilha   
+    # Lê o input do usuário para x
+    li $v0, 5                   # faz o setup para ler input
+    syscall                     # lê o input do usuário para x
+    move $t0, $v0               # guarda o input do usuário em t0
     
-    sw $ra, 0($sp)		 # Coloca $ra na pilha
+    li $v0, 4                    
+    la $a0, prompt5              # printa prompt para usuário inserir i
+    syscall
     
+    # Lê o input do usuário para i
+    li $v0, 5                   # faz o setup para ler input
+    syscall                     # lê o input do usuário para i
+    move $t1, $v0               # Store the input value of i in $t1
     
-    jal sqrt			 # chama a recursão
+    addi $sp, $sp, -12          # aloca espaço no sp para os dois valores e a pilha
+    
+    # manda os valores inseridos pelo usuário para a pilha
+    sw $t0, 8($sp)              # aloca o x na pilha
+    sw $t1, 4($sp)              # aloca o i na pilha
+    
+    sw $ra, 0($sp)              # aloca o ra na pilha
+    
+    jal sqrt                    # chama a funcao para comecar a recursão
     
     lw $ra, 0($sp)		 # recupera $ra
-    addi $sp, $sp, 12		 # adiciona os 16 bytes de volta na pilha           
+    addi $sp, $sp, 12		 # adiciona os 12 bytes de volta na pilha           
 
-    # Print result
+    # Print resultado
     move	$a0, $v0	 # move resultado para v0 para imprimir
     li $v0, 1                    # syscall: print int
     syscall
 
-    # Exit program
+    # Termina o programa
     li $v0, 10                   # syscall: exit
     syscall
 
 sqrt:
-    lw $a0, 8($sp)               # Load x from the stack
-    lw $a1, 4($sp)               # Load i from the stack
-    bgtz $a1, sqrt_nr            # Branch if i is greater than 0
-    li $v0, 1                    # Return 1 if i is 0 or negative
-    jr $ra                       # Return to the caller
+    lw $a0, 8($sp)               # carrega o x da pilha
+    lw $a1, 4($sp)               # carrega o i da pilha
+    bgtz $a1, sqrt_nr            # checa se o i é maior ou igual a 0
+    li $v0, 1                    # retorna 1 se i = 0
+    jr $ra                       # retorna quem chamou a função
 
 sqrt_nr:
-    addi $a1, $a1, -1            # Decrement i by 1
-    addi $sp, $sp, -12           # Allocate stack space for x, i, and return address
-    sw $a0, 8($sp)               # Save x on the stack
-    sw $a1, 4($sp)               # Save i on the stack
-    sw $ra, 0($sp)            	 # Save return address on the stack
-    jal sqrt                  	 # Recursive call
+    addi $a1, $a1, -1            # decrementa i por 1
+    addi $sp, $sp, -12           # aloca espaço para x, i e sp
+    sw $a0, 8($sp)               # salva o x na pilha
+    sw $a1, 4($sp)               # salva o i na pilha
+    sw $ra, 0($sp)            	 # salva o endereço de quem chamou na pilha
+    jal sqrt                  	 # chamada recursiva para chegar no fim da recursão
     
-    lw $ra, 0($sp)            	 # Restore return address
-    lw $a1, 4($sp)               # Restore i
-    lw $a0, 8($sp)           	 # Restore x
-    addi $sp, $sp, 12        	 # Deallocate stack space
+    lw $ra, 0($sp)            	 # recupera o endereço
+    lw $a1, 4($sp)               # recupera o i
+    lw $a0, 8($sp)           	 # recupera o x
+    addi $sp, $sp, 12        	 # libera o espaço da pilha
     
-    div $a0, $a0, $v0         # Calculate x / sqrt_nr(x, i - 1)
-    add $a0, $a0, $v0         # Add the result of the recursive call
-    srl $a0, $a0, 1           # Divide by 2 (shift right logical)
-    move $v0, $a0             # Move the result to $v0
-    jr $ra                    # Return to the caller
+    div $a0, $a0, $v0         # calcula x / sqrt_nr(x, i - 1)
+    add $a0, $a0, $v0         # adiciona o resultado da chamada recursiva
+    srl $a0, $a0, 1           # usa shift right pra divisão por 2
+    move $v0, $a0             # move o resultado pra v0
+    jr $ra                    # retorna pra quem fez a chamada
